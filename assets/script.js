e1fe52b4b21e6180eb9beb3aa26aeed9d72acd08
Alex Pierirodov <pererodovsasha2362000@gmail.com>
2025-10-14 14:20:27 +0300

    Refactor draggable functionality in script.js by encapsulating logic in a reusable function; enhance slider interaction for pricing cards with customizable speed parameter.

--- assets/script.js
+++ assets/script.js
@@ -81,34 +81,45 @@ window.addEventListener("DOMContentLoaded", () => {
     }
 
     counterInit();
-    const slider = document.querySelector('.pricing__cards__wrapper');
 
-    let isDown = false;
-    let startX;
-    let scrollLeft;
+    function dragInit({
+        selector,
+        speed
+    }) {
+        const slider = document.querySelector(selector);
 
-    slider.addEventListener('mousedown', (e) => {
-        isDown = true;
-        slider.classList.add('active');
-        startX = e.pageX - slider.offsetLeft;
-        scrollLeft = slider.scrollLeft;
-    });
+        let isDown = false;
+        let startX;
+        let scrollLeft;
 
-    slider.addEventListener('mouseleave', () => {
-        isDown = false;
-        slider.classList.remove('active');
-    });
+        slider.addEventListener('mousedown', (e) => {
+            isDown = true;
+            slider.classList.add('active');
+            startX = e.pageX - slider.offsetLeft;
+            scrollLeft = slider.scrollLeft;
+        });
 
-    slider.addEventListener('mouseup', () => {
-        isDown = false;
-        slider.classList.remove('active');
-    });
+        slider.addEventListener('mouseleave', () => {
+            isDown = false;
+            slider.classList.remove('active');
+        });
 
-    slider.addEventListener('mousemove', (e) => {
-        if (!isDown) return;
-        e.preventDefault();
-        const x = e.pageX - slider.offsetLeft;
-        const walk = (x - startX) * 10;
-        slider.scrollLeft = scrollLeft - walk;
+        slider.addEventListener('mouseup', () => {
+            isDown = false;
+            slider.classList.remove('active');
+        });
+
+        slider.addEventListener('mousemove', (e) => {
+            if (!isDown) return;
+            e.preventDefault();
+            const x = e.pageX - slider.offsetLeft;
+            const walk = (x - startX) * speed;
+            slider.scrollLeft = scrollLeft - walk;
+        });
+    }
+
+    dragInit({
+        selector: '.pricing__cards__wrapper',
+        speed: 10
     });
 });
